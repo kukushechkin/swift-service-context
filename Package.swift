1d6c845be1a0ad9ad4cb0268bb1eadd86b11a7fb
Vladimir Kukushkin <kukushechkin@mac.com>
2026-05-05 16:42:17 +0100

    Enable Explicit Sendable

--- Package.swift
+++ Package.swift
@@ -45,6 +45,6 @@ let package = Package(
 
 for target in package.targets {
     var settings = target.swiftSettings ?? []
-    settings.append(.enableExperimentalFeature("StrictConcurrency=complete"))
+    settings.append(.unsafeFlags(["-require-explicit-sendable"]))
     target.swiftSettings = settings
 }
